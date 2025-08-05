48579d5c9767f22ef422bb6fdcdd07cabd47666a
rupak1005 <rupak1005saini@gmail.com>
2025-08-06 01:37:31 +0530

    postgres setup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,14 @@ import Link from 'next/link'
 import React from 'react'
 import MobileNavbar from './MobileNavbar'
 import DesktopNavbar from './DesktopNavbar'
+import { currentUser } from '@clerk/nextjs/server';
+import { syncUser } from '@/actions/user.action';
 
-const Navbar = () => {
+ async function Navbar() {
+  const user = await currentUser();
+  if(user) await syncUser();
   return (
+
 <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
 <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
