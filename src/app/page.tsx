484365c066283db398041a14f2f4d7028662bb01
rupak1005 <rupak1005saini@gmail.com>
2025-08-06 00:05:58 +0530

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton, UserProfile } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (<>
+    <div>main page</div>
+    
+</>
+  );
+}
