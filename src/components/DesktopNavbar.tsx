49f57e2beda946715a0d59a6ee411f2fa0ee5ddc
rupak1005 <rupak1005saini@gmail.com>
2025-08-06 17:02:41 +0530

    Refactor DesktopNavbar to use ModeToggle instead of ThemeToggleButton; remove unused theme-related components and animations.

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -2,16 +2,16 @@ import { BellIcon, HomeIcon, UserIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { SignInButton, UserButton } from "@clerk/nextjs";
-// import ModeToggle from "./ModeToggle";
+import ModeToggle from "./ModeToggle";
 import { currentUser } from "@clerk/nextjs/server";
-import ThemeToggleButton from "./ui/theme-toggle-button";
+
 
 async function DesktopNavbar() {
   const user = await currentUser();
 
   return (
     <div className="hidden md:flex items-center space-x-4">
-      <ThemeToggleButton />
+      <ModeToggle />
 
       <Button variant="ghost" className="flex items-center gap-2" asChild>
         <Link href="/">
